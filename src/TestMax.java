27f10feeb3f64ad170179cec21d833000698e647
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-10-04 14:32:56 +0200

    Få kommentarer

---
+++ src/TestMax.java
@@ -0,0 +1,21 @@
+//Class kaldet TestMax
+
+public class TestMax {
+    public static void main(String[] args) {
+// variablerne i, j, k instantieres
+        
+        int i = 5;
+        int j = 2;
+        int k = max(i, j);
+        System.out.println("The maximum of " + i +
+                " and " + j + " is " + k);
+    }
+    public static int max(int num1, int num2) {
+        int result;
+        if (num1 > num2);
+            else
+            result = num1;
+        result = num2;
+        return result;
+    }
+}
